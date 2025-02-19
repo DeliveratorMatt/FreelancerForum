62c57894e4e7e4ad408f3b09bd9c8413e86d62e4
Matt Weber <mathteacherone@gmail.com>
2025-02-19 09:49:52 -0500

    finished

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const PRICE_RANGE = { min: 20, max: 200 };
 const NUM_FREELANCERS = 100;
 
 // ===State===
-function FLStats() {
+function MakeFL() {
   const FLName = Math.floor(Math.random() * 5);
   const FLOcc = Math.floor(Math.random() * 5);
   const FLRate = Math.floor(Math.random() * 180) + 20;
@@ -24,7 +24,7 @@ function FLStats() {
   return singleFL;
 }
 
-let allLancers = Array.from({ length: NUM_FREELANCERS }, FLStats);
+let allLancers = Array.from({ length: NUM_FREELANCERS }, MakeFL);
 
 let sum = 0;
 for (const lancer of allLancers) {
@@ -34,26 +34,28 @@ const meanRate = sum / NUM_FREELANCERS;
 
 // ===Components===
 function OneFL(FLStats) {
-  const { Name, Occupation, Price } = singleFL;
+  const { name, occupation, price } = FLStats;
 
-  const $singleFL = document.createElement("figure");
-  $singleFL.classList.add("single");
-  $singleFL.innerHTML = `
-    <p>Name: ${Name}</p>
-    <p>Occupation: ${Occupation}</p>
-    <p>Price: ${Price}</p>`;
-  return $singleFL;
+  const $SingleFL = document.createElement("figure");
+  $SingleFL.classList.add("single");
+  $SingleFL.innerHTML = `
+    <p>Name: ${name}</p>
+    <p>Occupation: ${occupation}</p>
+    <p>Price: ${price}</p>`;
+  return $SingleFL;
 }
 
 function FLArray() {
   const $FLarray = document.createElement("article");
   $FLarray.classList.add("lancers");
-  const $contents = freeLancers.map(OneFL);
+  //Take the array of all lancers and map them into contents
+  //one at a time using the OneFL function.
+  const $contents = allLancers.map(OneFL);
   $FLarray.replaceChildren(...$contents);
   return $FLarray;
 }
 
-function avgRate() {
+function AvgRate() {
   const $avgDisplay = document.createElement("section");
   $avgDisplay.replaceChildren(...meanRate);
   return $avgDisplay;
@@ -65,19 +67,9 @@ function render() {
   $app.innerHTML = `
   <h1>Freelancers</h1>
   <FLArray></FLArray>
-  <avgRate></avgRate>
+  <AvgRate></AvgRate>
   `;
   $app.querySelector("FLArray").replaceWith(FLArray());
-  $app.querySelector("avgRate").replaceWith(avgRate());
+  $app.querySelector("AvgRate").replaceWith(AvgRate());
 }
 render();
-
-/*function render() {
-  const $app = document.querySelector("#app");
-  $app.innerHTML = `
-    <h1>Inspirational Quotes</h1>
-    <QuoteCards></QuoteCards>
-  `;
-  $app.querySelector("QuoteCards").replaceWith(QuoteCards(quotes));
-}
-render();*/
